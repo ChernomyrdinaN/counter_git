34f59d717f3c3a7ce1e104abe063e033f12b82fe
Nata <chernomyrdina17@gmail.com>
2025-01-06 19:22:21 +0500

    Финальная задача(усложненный вариант)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    private var counter: Int = 0 // значение переменной можно задать через сториборд
+    
+    private func getDate() -> String { 
+        let date = Date.now
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let formatDate = dateFormatter.string(from: date)
+        
+        return formatDate
+    }
+    
+    lazy var formattedDate = getDate() // "ленивое" свойство для получения текущей даты и времени
+    
+    @IBOutlet weak var clickPlusButton: UIButton!
+    @IBOutlet weak var clickMinusButton: UIButton!
+    @IBOutlet weak var clickResetButton: UIButton!
+    @IBOutlet weak var changesTextView: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = String(counter)
+        changesTextView.text = "История изменений: \n"
     }
-
-
+    
+    @IBAction func clickPlusButton(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+        changesTextView.text += "\(getDate()) значение изменено на +1\n"
+    }
+    @IBAction func clickMinusButton(_ sender: Any) {
+        if counter > 0 {
+           counter -= 1
+           counterLabel.text = "Значение счётчика: \(counter)"
+           changesTextView.text += "\(getDate()) значение изменено на -1\n"
+            } else {changesTextView.text += "\(getDate()) попытка уменьшить значение счётчика ниже 0\n"}
+    }
+    
+    @IBAction func clickResetButton(_ sender: Any) {
+        counter = 0
+        counterLabel.text = String(counter)
+        changesTextView.text += "\(getDate()): значение сброшено\n"
+    }
 }
+
+
+
+
 
