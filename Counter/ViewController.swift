a8ab2e2eaca6df01afa3a734177711e6945bea59
ChernomyrdinaN <chernomyrdina17@gmail.com>
2025-01-10 12:23:26 +0300

    Исправлено после ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private var counter: Int = 0 // значение переменной можно задать через сториборд
-    
-    private func getDate() -> String { 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        counterLabel.text = String(counter)
+        changesTextView.text = "История изменений: \n"
+    }
+        private func getDate() -> String { 
         let date = Date.now
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
@@ -20,34 +24,27 @@ class ViewController: UIViewController {
         return formatDate
     }
     
-    lazy var formattedDate = getDate() // "ленивое" свойство для получения текущей даты и времени
+    lazy private var formattedDate = getDate() // "ленивое" свойство для получения текущей даты и времени
+
+    @IBOutlet private weak var clickPlusButton: UIButton!
+    @IBOutlet private weak var clickMinusButton: UIButton!
+    @IBOutlet private weak var clickResetButton: UIButton!
+    @IBOutlet private weak var changesTextView: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
     
-    @IBOutlet weak var clickPlusButton: UIButton!
-    @IBOutlet weak var clickMinusButton: UIButton!
-    @IBOutlet weak var clickResetButton: UIButton!
-    @IBOutlet weak var changesTextView: UITextView!
-    @IBOutlet weak var counterLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        counterLabel.text = String(counter)
-        changesTextView.text = "История изменений: \n"
-    }
-    
-    @IBAction func clickPlusButton(_ sender: Any) {
+    @IBAction private func clickPlusButton(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика: \(counter)"
         changesTextView.text += "\(getDate()) значение изменено на +1\n"
     }
-    @IBAction func clickMinusButton(_ sender: Any) {
+    @IBAction private func clickMinusButton(_ sender: Any) {
         if counter > 0 {
            counter -= 1
            counterLabel.text = "Значение счётчика: \(counter)"
            changesTextView.text += "\(getDate()) значение изменено на -1\n"
             } else {changesTextView.text += "\(getDate()) попытка уменьшить значение счётчика ниже 0\n"}
-    }
-    
-    @IBAction func clickResetButton(_ sender: Any) {
+    }    
+    @IBAction private func clickResetButton(_ sender: Any) {
         counter = 0
         counterLabel.text = String(counter)
         changesTextView.text += "\(getDate()): значение сброшено\n"
